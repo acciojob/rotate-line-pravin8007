3f66b9527e017c50515be6d9bc8ffeab609a7535
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2024-01-30 18:20:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ const line = document.getElementById('line');
+  let angle = 0;
+
+  function rotateLine() {
+    angle += 2;
+    line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+    requestAnimationFrame(rotateLine);
+  }
+
+  rotateLine();
